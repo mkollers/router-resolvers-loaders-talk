84322b8caf479f070619a526460eb5e2691b863c
Markus Kollers <m.kollers@vertical.de>
2018-07-17 13:28:52 +0200

    Go back to #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { merge, Observable } from 'rxjs';
+import { filter, mapTo } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isRouting$: Observable<boolean>;
 
-  constructor() { }
+  constructor(
+    router: Router
+  ) {
+    const navigationStart$ = router.events.pipe(
+      filter(event => event instanceof NavigationStart),
+      mapTo(true)
+    );
+    const navigationEnd$ = router.events.pipe(
+      filter(event => event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError),
+      mapTo(false)
+    );
+    this.isRouting$ = merge(navigationStart$, navigationEnd$).pipe();
+  }
 }
