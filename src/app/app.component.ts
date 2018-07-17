0eadc32645984ba2c27a2cf10ae8ab1c7c625d38
Markus Kollers <m.kollers@vertical.de>
2018-07-17 13:21:37 +0200

    Rollback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
-import { merge, Observable } from 'rxjs';
-import { filter, mapTo } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +6,6 @@ import { filter, mapTo } from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isRouting$: Observable<boolean>;
 
-  constructor(
-    router: Router
-  ) {
-    const navigationStart$ = router.events.pipe(
-      filter(event => event instanceof NavigationStart),
-      mapTo(true)
-    );
-    const navigationEnd$ = router.events.pipe(
-      filter(event => event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError),
-      mapTo(false)
-    );
-    this.isRouting$ = merge(navigationStart$, navigationEnd$).pipe();
-  }
+  constructor() { }
 }
